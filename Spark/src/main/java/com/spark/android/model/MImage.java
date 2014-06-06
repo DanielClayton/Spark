0151aee60d27cc76f1b31d6d1fd07ffd13e44f9b
Daniel Clayton <daniel@twotaplabs.com>
2014-06-06 08:54:39 -0400

    Made MImage implement Serializable.

--- Spark/src/main/java/com/spark/android/model/MImage.java
+++ Spark/src/main/java/com/spark/android/model/MImage.java
@@ -1,9 +1,11 @@
 package com.spark.android.model;
 
+import java.io.Serializable;
+
 /**
  * Created by danielclayton on 6/5/14.
  */
-public class MImage {
+public class MImage implements Serializable {
     private String thumb;
 
     public String getThumb() {
